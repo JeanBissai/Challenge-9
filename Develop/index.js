4936f44c301fbf4394315f6751fe75904a3159eb
Your Name <you@example.com>
2022-12-04 20:31:31 -0700

    init

---
+++ Develop/index.js
@@ -0,0 +1,23 @@
+// TODO: Include packages needed for this application
+
+const { default: inquirer } = require("inquirer");
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    FileSystem.writeToFile(fileName,generateMarkdown(data) function (err) {
+        if (err)){
+            return console.log(err);
+        }
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then((data)) =>{
+        console.log(JSON.stringify(data, null ""))
+    }}
+
+// Function call to initialize app
+init();
